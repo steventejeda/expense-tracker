cef265d7ba764cf8db9455ffd847114074dead34
Steven Tejeda <steventejeda@gmail.com>
2021-04-23 01:32:31 -0400

    Added remaining components. Created context folder that obtained reducer & GlobalState

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
+
+import { GlobalProvider } from './context/GlobalState';
+
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -17,7 +20,7 @@ function App() {
         <AddTransaction />
 
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
